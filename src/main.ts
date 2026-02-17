a703fc8f52d07696fe1d78b80c2bd0a2e8f52265
Jonathan Attram <codinghub242@gmail.com>
2026-02-17 17:08:16 +0000

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,23 @@
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+// import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+// import { provideHttpClient } from '@angular/common/http';
+
+// import { routes } from './app/app.routes';
+// import { AppComponent } from './app/app.component';
+
+// bootstrapApplication(AppComponent, {
+//   providers: [
+//     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+//     provideIonicAngular(),
+//     provideRouter(routes, withPreloading(PreloadAllModules)),
+//     provideHttpClient(),
+//   ],
+// });
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { provideHttpClient } from '@angular/common/http';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -10,7 +26,7 @@ bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
     provideHttpClient(),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
   ],
 });
