abdd8781f21b2175105dee718572d2dcd7ae9de1
Jonathan Attram <codinghub242@gmail.com>
2026-02-18 17:33:03 +0000

    po

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { homeOutline, peopleOutline, businessOutline, trendingUpOutline, checkboxOutline, calendarOutline, cubeOutline, documentTextOutline, receiptOutline, logOutOutline, menuOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,20 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    // Register global icons
+    addIcons({
+      homeOutline,
+      peopleOutline,
+      businessOutline,
+      trendingUpOutline,
+      checkboxOutline,
+      calendarOutline,
+      cubeOutline,
+      documentTextOutline,
+      receiptOutline,
+      logOutOutline,
+      menuOutline
+    });
+  }
 }
