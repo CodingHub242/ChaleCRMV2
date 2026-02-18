a6ef19f33762edfcb8e4ffe076251094285b37c9
Jonathan Attram <codinghub242@gmail.com>
2026-02-18 17:10:14 +0000

    Update dashboard.page.ts

--- src/app/features/dashboard/dashboard.page.ts
+++ src/app/features/dashboard/dashboard.page.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar,IonButton,IonButtons,IonMenuButton,IonIcon,IonRow,IonCol,IonModal } from '@ionic/angular/standalone';
-import { RouterModule } from '@angular/router';
+import { RouterModule, RouterLink } from '@angular/router';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonButtons, IonMenuButton, IonIcon, IonRow, IonCol, IonModal } from '@ionic/angular/standalone';
 import { IonicModule, ModalController } from '@ionic/angular';
 import { ApiService } from '../../core/services/api.service';
 import { AuthService } from '../../core/services/auth.service';
@@ -32,7 +32,7 @@ interface CalendarEvent {
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar,IonButton,IonButtons,IonIcon,IonModal,IonRow,IonCol,IonMenuButton, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar,IonButton,IonButtons,IonIcon,IonModal,IonRow,IonCol,IonMenuButton, CommonModule, FormsModule, RouterModule]
 })
 export class DashboardPage implements OnInit {
 stats: any = {
