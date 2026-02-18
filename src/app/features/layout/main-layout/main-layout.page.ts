67be8a01b43d378c21348e509915bab93efb1a30
Jonathan Attram <codinghub242@gmail.com>
2026-02-18 16:48:12 +0000

    main

--- src/app/features/layout/main-layout/main-layout.page.ts
+++ src/app/features/layout/main-layout/main-layout.page.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { IonicModule, MenuController, NavController } from '@ionic/angular';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { AuthService } from '../../../core/services/auth.service';
+import { addIcons } from 'ionicons';
+import { briefcase,add, trash, create, mail, document, close, eye, download, checkmark, arrowBack, arrowUp, arrowDown, filter, cloudUpload, checkmarkCircle, layers, time, alertCircle, chevronBack, chevronForward, chevronDown, person, logOut, list, calendar, analytics, trendingUp, flag, folderOpen, ellipse, business, notificationsOutline, settingsOutline, cash, people, businessOutline, homeOutline, peopleOutline, trendingUpOutline, checkboxOutline, calendarOutline, cubeOutline, documentTextOutline, receiptOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-main-layout',
@@ -11,10 +16,63 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class MainLayoutPage implements OnInit {
+ currentUser: any;
+  sidebarOpen = false;
+  isMobile = false;
 
-  constructor() { }
+  constructor(
+    private menuController: MenuController,
+    private authService: AuthService,
+    private router: Router
+  ) {
+      addIcons({briefcase,notificationsOutline,settingsOutline,trendingUp,cash,chevronBack,chevronForward,chevronDown,alertCircle, add, trash, cubeOutline,documentTextOutline,receiptOutline, mail, document, close, eye, download, checkmark, arrowBack, arrowUp, arrowDown, checkboxOutline,calendarOutline,checkmarkCircle,cloudUpload,layers,time,person,logOut,list,calendar,analytics,people,flag,homeOutline,peopleOutline,trendingUpOutline,ellipse,businessOutline});
+     }
 
-  ngOnInit() {
+ ngOnInit(): void {
+    this.authService.currentUser$.subscribe(user => {
+      this.currentUser = user;
+    });
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    this.isMobile = window.innerWidth < 768;
+    // On desktop, sidebar is always visible
+    if (!this.isMobile) {
+      this.sidebarOpen = true;
+    }
+  }
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  closeSidebar() {
+    this.sidebarOpen = false;
+  }
+
+  closeSidebarOnMobile() {
+    if (this.isMobile) {
+      this.sidebarOpen = false;
+    }
+  }
+
+  getUserInitials(): string {
+    if (!this.currentUser?.name) return '?';
+    const names = this.currentUser.name.split(' ');
+    if (names.length >= 2) {
+      return (names[0][0] + names[1][0]).toUpperCase();
+    }
+    return names[0].substring(0, 2).toUpperCase();
+  }
+
+  logout(): void {
+    this.authService.logout();
   }
 
 }
