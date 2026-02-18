ba5086a2e5834f1c65e3d3c2afe2530194b0de1f
Jonathan Attram <codinghub242@gmail.com>
2026-02-18 17:02:59 +0000

    Update dashboard.page.ts

--- src/app/features/dashboard/dashboard.page.ts
+++ src/app/features/dashboard/dashboard.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar,IonButton,IonButtons,IonMenuButton } from '@ionic/angular/standalone';
 import { RouterModule } from '@angular/router';
 import { IonicModule, ModalController } from '@ionic/angular';
 import { ApiService } from '../../core/services/api.service';
@@ -32,7 +32,7 @@ interface CalendarEvent {
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar,IonButton,IonButtons,IonMenuButton, CommonModule, FormsModule]
 })
 export class DashboardPage implements OnInit {
 stats: any = {
