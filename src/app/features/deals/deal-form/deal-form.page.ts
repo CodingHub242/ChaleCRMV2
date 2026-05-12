576a4baa29b56922071ae7eeb756a13f809d980f
Jonathan Attram <codinghub242@gmail.com>
2026-05-12 19:02:19 +0000

    Update deal-form.page.ts

--- src/app/features/deals/deal-form/deal-form.page.ts
+++ src/app/features/deals/deal-form/deal-form.page.ts
@@ -222,7 +222,7 @@ export class DealFormPage implements OnInit {
 
     modal.onDidDismiss().then((result) => {
       if (result.data) {
-        console.log('Selected contact:', result.data);
+       // console.log('Selected contact:', result.data);
         this.deal.contact_id = result.data.id;
       }
     });
